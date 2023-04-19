d034b3baf0b29413434451d17f488a6f29de27ab
Alex Mefisto <alexandr.mefisto@poly-tech.com.ua>
2023-04-19 09:24:52 +0300

    Initial Commit

---
+++ CollectionViewTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTutorial
+//
+//  Created by Alexandr Mefisto on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
